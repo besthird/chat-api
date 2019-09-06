ccfa3838423deecf741d68c4631883419ca39b32
李铭昕 <715557344@qq.com>
2019-09-06 11:04:14 +0800

    增加用户注册脚本

---
+++ app/Command/UserRegistCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace App\Command;
+
+use App\Model\User;
+use Hyperf\Command\Annotation\Command;
+use Hyperf\Command\Command as HyperfCommand;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+/**
+ * @Command
+ */
+class UserRegistCommand extends HyperfCommand
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+
+        parent::__construct('user:regist');
+    }
+
+    public function configure()
+    {
+        $this->setDescription('注册用户信息');
+        $this->addArgument('token', InputArgument::REQUIRED, '用户TOKEN');
+        $this->addOption('name', 'N', InputOption::VALUE_REQUIRED, '用户名');
+    }
+
+    public function handle()
+    {
+        $token = $this->input->getArgument('token');
+        $name = $this->input->getOption('name');
+        if (empty($name)) {
+            $this->error('请输入用户名');
+            return;
+        }
+
+        $user = new User();
+        $user->token = $token;
+        $user->name = $name;
+        $user->save();
+
+        $this->info(sprintf('用户创建完毕，请使用 %s 登录系统', $token));
+    }
+}
---
+++ app/Model/User.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+namespace App\Model;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $token
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ */
+class User extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'user';
+
+    /**
+     * The connection name for the model.
+     *
+     * @var string
+     */
+    protected $connection = 'default';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['id', 'name', 'token', 'created_at', 'updated_at'];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = ['id' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+}
